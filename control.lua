87eab21d94f42a4963565232c78d503d944e53c4
Sam Kemp <sam@samkemp.me>
2021-06-05 11:50:11 +0100

    Fixed name change

--- control.lua
+++ control.lua
@@ -7,9 +7,9 @@ script.on_event("sams-testing-input", function(event)
     local player_inventory = player.character.get_inventory(defines.inventory.character_main)
 
     player_inventory.insert({name = "steel-chest", count=50})
-    player_inventory.insert({name = "defener-spawner", count=25})
-    -- player_inventory.insert({name = "distractor-capsule", count=25})
-    -- player_inventory.insert({name = "destroyer-capsule", count=25})
+    player_inventory.insert({name = "defener-spawner-equipment", count=25})
+    player_inventory.insert({name = "distractor-spawner-equipment", count=25})
+    player_inventory.insert({name = "destroyer-spawner-equipment", count=25})
 
     player.force.technologies['follower-robot-count-1'].researched=true
     player.force.technologies['follower-robot-count-2'].researched=true
