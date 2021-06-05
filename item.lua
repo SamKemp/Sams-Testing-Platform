9b3435f1227a195694ea7b38dddb3b58631ca35e
Sam Kemp <sam@samkemp.me>
2021-06-05 10:22:24 +0100

    Fixing Defender Spawner?

---
+++ item.lua
@@ -0,0 +1,17 @@
+local defenderSpawner = table.deepcopy(data.raw["belt-immunity-equipment"]["belt-immunity-equipment"])
+
+defenderSpawner.name = "defener-spawner"
+defenderSpawner.icons = {
+    {
+        icon = data.raw["capsule"]["defender-capsule"].icon,
+        tint = {r=1,g=0,b=0,a=0.3}
+    },
+}
+
+local recipe = table.deepcopy(data.raw["recipe"]["belt-immunity-equipment"])
+recipe.enabled = true
+recipe.name = "defener-spawner"
+recipe.ingredients = {{"defender-capsule",20},{"advanced-circuit",5},{"steel-plate",10}}
+recipe.result = "defener-spawner"
+
+data:extend{defenderSpawner,recipe}
