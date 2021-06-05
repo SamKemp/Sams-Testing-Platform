98ecc17d0944b04c7fc1cd75a434a6334e6e2588
Sam Kemp <sam@samkemp.me>
2021-06-05 12:05:03 +0100

    force tech

--- control.lua
+++ control.lua
@@ -6,15 +6,18 @@ script.on_event("sams-testing-input", function(event)
 
     local player_inventory = player.character.get_inventory(defines.inventory.character_main)
 
-    player_inventory.insert({name = "steel-chest", count=50})
-    player_inventory.insert({name = "defener-spawner-equipment", count=25})
-    player_inventory.insert({name = "distractor-spawner-equipment", count=25})
-    player_inventory.insert({name = "destroyer-spawner-equipment", count=25})
+    --player_inventory.insert({name = "steel-chest", count=50})
+    --player_inventory.insert({name = "defener-spawner-equipment", count=25})
+    --player_inventory.insert({name = "distractor-spawner-equipment", count=25})
+    --player_inventory.insert({name = "destroyer-spawner-equipment", count=25})
 
     player.force.technologies['follower-robot-count-1'].researched=true
     player.force.technologies['follower-robot-count-2'].researched=true
     player.force.technologies['follower-robot-count-3'].researched=true
     player.force.technologies['follower-robot-count-4'].researched=true
+    player.force.technologies['follower-robot-count-5'].researched=true
+    player.force.technologies['follower-robot-count-6'].researched=true
+    player.force.technologies['follower-robot-count-7'].researched=true
 end
 )
 
