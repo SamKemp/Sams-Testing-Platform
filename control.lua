ed808e01bf66c38d98b59fb0a6becb69256c5e7b
Sam Kemp <sam@samkemp.me>
2021-06-05 10:17:32 +0100

    Added Defedner Spawner Equipment

--- control.lua
+++ control.lua
@@ -17,3 +17,26 @@ script.on_event("sams-testing-input", function(event)
     player.force.technologies['follower-robot-count-4'].researched=true
 end
 )
+
+script.on_event(defines.events.on_player_changed_position,
+  function(event)
+    local player = game.get_player(event.player_index) -- get the player that moved
+    local armorInventory = player.get_inventory(defines.inventory.character_armor) -- gets the armor slot of the player
+    local armorGrid = nil
+
+    if armorInventory.find_item_stack("power-armor") then
+        armorGrid = armorInventory.find_item_stack("power-armor").grid
+    end
+    if armorInventory.find_item_stack("power-armor-mk2") then
+        armorGrid = armorInventory.find_item_stack("power-armor-mk2").grid
+    end
+    if armorInventory.find_item_stack("modular-armor") then
+        armorGrid = armorInventory.find_item_stack("modular-armor").grid
+    end
+
+    if armorGrid != nil then
+        armorGrid.get_item_count("defener-spawner") >= 1 then
+            player.surface.create_entity{name="defender-capsule", position=player.position, force="neutral"}
+        end
+    end
+)
--- data.lua
+++ data.lua
@@ -7,3 +7,21 @@ local button={
     consuming = "none"
 }
 data:extend{button}
+
+local defenderSpawner = table.deepcopy(data.raw["belt-immunity-equipment"]["belt-immunity-equipment"])
+
+defenderSpawner.name = "defener-spawner"
+defenderSpawner.icons = {
+    {
+        icon = data.raw["capsule"]["defender-capsule"].icon,
+        tint = {r=1,g=0,b=0,a=0.3}
+    },
+}
+
+local defenderSpawner_Recipe = table.deepcopy(data.raw["recipe"]["belt-immunity-equipment"])
+defenderSpawner_Recipe.enabled = true
+defenderSpawner_Recipe.name = "defener-spawner"
+defenderSpawner_Recipe.ingredients = {{"defender-capsule",20},{"advanced-circuit",5},{"steel-plate",10}}
+defenderSpawner_Recipe.result = "defener-spawner"
+
+data:extend{defenderSpawner,defenderSpawner_Recipe}
