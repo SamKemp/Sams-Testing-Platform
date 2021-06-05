3a1229ca6fb3b3d9f8995d66fb0ab4910a0a00c6
Sam Kemp <sam@samkemp.me>
2021-06-05 09:32:58 +0100

    Fixed defender names and added research force

--- control.lua
+++ control.lua
@@ -7,8 +7,13 @@ script.on_event("sams-testing-input", function(event)
     local player_inventory = player.character.get_inventory(defines.inventory.character_main)
 
     player_inventory.insert({name = "steel-chest", count=50})
-    player_inventory.insert({name = "defender", count=25})
-    player_inventory.insert({name = "distractor", count=25})
-    player_inventory.insert({name = "destroyer", count=25})
+    player_inventory.insert({name = "defender-capsule", count=25})
+    player_inventory.insert({name = "distractor-capsule", count=25})
+    player_inventory.insert({name = "destroyer-capsule", count=25})
+
+    player.force.technologies['follower-robot-count-1'].researched=true
+    player.force.technologies['follower-robot-count-2'].researched=true
+    player.force.technologies['follower-robot-count-3'].researched=true
+    player.force.technologies['follower-robot-count-4'].researched=true
 end
 )
