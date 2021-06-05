a43e35f47e386031b4aba85ee9f4603b1f4fcbe0
Sam Kemp <sam@samkemp.me>
2021-06-05 09:26:35 +0100

    Added inserts

--- control.lua
+++ control.lua
@@ -3,5 +3,12 @@
 script.on_event("sams-testing-input", function(event)
     local player = game.get_player(event.player_index)
     game.print("Player " .. player.name .. " Ran on tick: " .. tostring(event.tick))
+
+    local player_inventory = player.character.get_inventory(defines.inventory.character_main)
+
+    player_inventory.insert({name = "steel-chest", count=50})
+    player_inventory.insert({name = "defender", count=25})
+    player_inventory.insert({name = "distractor", count=25})
+    player_inventory.insert({name = "destroyer", count=25})
 end
 )
